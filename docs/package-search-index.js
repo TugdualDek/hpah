35f1689e2f87b467232576ae3194dbb03ae64635
Tugdual de K <35851118+TugdualDek@users.noreply.github.com>
2023-05-13 17:44:30 +0200

    Delete docs directory

--- docs/package-search-index.js
+++
@@ -1,5 +0,0 @@
-packageSearchIndex = [{
-    "l": "All Packages",
-    "u": "allpackages-index.html"
-}, {"l": "kerdrel.tugdual"}, {"l": "kerdrel.tugdual.characters"}, {"l": "kerdrel.tugdual.ressources"}, {"l": "kerdrel.tugdual.spells"}, {"l": "kerdrel.tugdual.tools"}, {"l": "kerdrel.tugdual.wizarding"}];
-updateSearchResults();
